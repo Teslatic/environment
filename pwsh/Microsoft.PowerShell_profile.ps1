02514254999bf0fa60986d900ace14ffeeff7202
Mike Evans <maevans@amd.com>
2021-04-17 13:39:05 -0400

    Add conda stuff to PSH profile.

--- pwsh/Microsoft.PowerShell_profile.ps1
+++ pwsh/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,19 @@
 Import-Module posh-git
 Import-Module oh-my-posh
 #Set-Theme Agnoster
-Set-PoshPrompt -Theme ~/.mikeev.minimal.omp.json
+#CONDA STUFF
+$Env:CONDA_EXE = "C:/Code/Apps/Miniconda\Scripts\conda.exe"
+$Env:_CE_M = ""
+$Env:_CE_CONDA = ""
+$Env:_CONDA_ROOT = "C:/Code/Apps/Miniconda"
+$Env:_CONDA_EXE = "C:/Code/Apps/Miniconda\Scripts\conda.exe"
+
+Import-Module "$Env:_CONDA_ROOT\shell\condabin\Conda.psm1"
+Add-CondaEnvironmentToPrompt
+#/CONDA STUFF
+
+Set-PoshPrompt -Theme C:\Code\environment\pwsh\.mikeev.jan.omp.json
+clear
+
+
+function update-powershell {iex "& { $(irm https://aka.ms/install-powershell.ps1) } -UseMSI"}
